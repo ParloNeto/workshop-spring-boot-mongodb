ee40f9351492f58b3a5f608fb218b4093791c0be
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2022-12-06 10:11:57 -0300

    Inserção de usuário com POST

--- src/main/java/com/pauloneto/workshopmongo/services/UserService.java
+++ src/main/java/com/pauloneto/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.pauloneto.workshopmongo.services;
 
 import com.pauloneto.workshopmongo.domain.User;
+import com.pauloneto.workshopmongo.dto.UserDTO;
 import com.pauloneto.workshopmongo.repository.UserRepository;
 import com.pauloneto.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
