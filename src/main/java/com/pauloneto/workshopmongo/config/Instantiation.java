803d2470e70a48d6a8756bfe61f78561bebf2287
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2022-12-06 12:00:15 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/pauloneto/workshopmongo/config/Instantiation.java
+++ src/main/java/com/pauloneto/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.pauloneto.workshopmongo.config;
 import com.pauloneto.workshopmongo.domain.Post;
 import com.pauloneto.workshopmongo.domain.User;
 import com.pauloneto.workshopmongo.dto.AuthorDTO;
+import com.pauloneto.workshopmongo.dto.CommentDTO;
 import com.pauloneto.workshopmongo.repository.PostRepository;
 import com.pauloneto.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +39,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/11/2022"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("22/11/2022"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/11/2021"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/11/2021"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/11/2021"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
