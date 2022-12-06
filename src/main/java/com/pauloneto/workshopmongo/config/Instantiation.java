abfaf606b4c8c299bad2ad2c623599374b2ba597
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2022-12-06 11:40:39 -0300

    Referenciando os posts do usuário

--- src/main/java/com/pauloneto/workshopmongo/config/Instantiation.java
+++ src/main/java/com/pauloneto/workshopmongo/config/Instantiation.java
@@ -38,8 +38,10 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/11/2022"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("22/11/2022"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
-        
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
