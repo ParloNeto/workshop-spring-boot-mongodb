41bab02cb43838ee8320544eee723ba1172e4287
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2022-12-09 01:13:28 -0300

    Consulta simples com query methods

--- src/main/java/com/pauloneto/workshopmongo/resources/PostResource.java
+++ src/main/java/com/pauloneto/workshopmongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.pauloneto.workshopmongo.resources;
 import com.pauloneto.workshopmongo.domain.Post;
 import com.pauloneto.workshopmongo.domain.User;
 import com.pauloneto.workshopmongo.dto.UserDTO;
+import com.pauloneto.workshopmongo.resources.util.URL;
 import com.pauloneto.workshopmongo.services.PostService;
 import com.pauloneto.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,13 @@ public class PostResource {
     public ResponseEntity <Post> findById(@PathVariable String id){
        Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity <List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
